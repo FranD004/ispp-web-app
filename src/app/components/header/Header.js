117f6e2a0d3a6a59e7a0a1a587488c435e7ec85c
Francisco Martino <franciscomartino22@gmail.com>
2025-06-10 19:53:14 -0300

    3re COMMINT

--- src/app/components/header/Header.js
+++ src/app/components/header/Header.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import Image from 'next/image'
 
 export default function Header() {
   return (
-    <header className='max-w-[1500px] m-auto px-[5rem] py-[2rem] relative pt-8 grid grid-cols-2 items-center gap-8'>
+    <header className='max-w-[1500px] m-auto px-[5rem] py-[2rem] relative pt-[2rem] grid grid-cols-2 items-center gap-8'>
 
       <div className='p-0 m-0 outline-0 border-0 border-box-border'>
 
-        <span className='absolute -z-100 shadow-custom-blur'></span>
-        <span className='absolute -z-50 blur-[length:1000px] bg-[#12710f]'></span>
+          <span className='absolute -z-100 shadow-2xl shadow-[#12710f]'/>
+          <span className='absolute -z-50 blur-[length:1000px] bg-[#12710f]'/>
         <h1 className='text-white mb-[1rem] text-[3rem] font-bold leading-[3rem]'>
           Instituto <span className='[text-transparent] [text-stroke:1px_#fff]'>Superior</span> Del Profesorado De Pascanas
         </h1>
@@ -21,7 +22,12 @@ export default function Header() {
       </div>
 
       <div>
-        <img src="" alt="" />
+        <Image 
+          src='/images/header.png' 
+          alt='Header Image'
+          width={600}
+          height={300}
+        />
       </div>
       
     </header>
--- src/app/components/navbar/Navbar.js
+++ src/app/components/navbar/Navbar.js
@@ -1,35 +1,62 @@
 'use client'
 import React from 'react'
+import Image from 'next/image'
 
 export default function Navbar() {
   return (
     <>
-      <nav className='max-w-auto m-auto pt-1 pb-2 flex items-center justify-between gap-2'>
+      <nav className='max-w-auto m-auto pt-1 pb-2 pr-2 pl-2 flex items-center justify-between gap-2'>
 
-        <img src='public/images/logo.png' className='max-w-[100px]'/>
-        
+        <Image 
+          src='/images/logo.png' 
+          alt='Logo'
+          width={100}
+          height={100}
+        />
+
         <ul className='list-none flex items-center gap-[3rem]'>
 
           <li className='relative p text-white'>
-            <a href='' className="relative pb-3 text-white after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-0 after:bg-[#1eca1b] after:transition-all after:duration-300 hover:after:w-[70%]">ADMINISTRATIVO</a>
+            <a 
+              href='' 
+              className="relative pb-3 text-white after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-0 after:bg-[#1eca1b] after:transition-all after:duration-300 hover:after:w-[70%]">
+                ADMINISTRATIVO</a>
           </li>
           <li className='relative p text-white'>
-            <a href='' className="relative pb-3 text-white after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-0 after:bg-[#1eca1b] after:transition-all after:duration-300 hover:after:w-[70%]">ASISTENCIA</a>
+            <a 
+              href='' 
+              className="relative pb-3 text-white after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-0 after:bg-[#1eca1b] after:transition-all after:duration-300 hover:after:w-[70%]">
+                ASISTENCIA</a>
           </li>
           <li className='relative p text-white'>
-            <a href='' className="relative pb-3 text-white after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-0 after:bg-[#1eca1b] after:transition-all after:duration-300 hover:after:w-[70%]">NOTAS</a>
+            <a 
+              href='' 
+              className="relative pb-3 text-white after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-0 after:bg-[#1eca1b] after:transition-all after:duration-300 hover:after:w-[70%]">
+                NOTAS</a>
           </li>
           <li className='relative p text-white'>
-            <a href='' className="relative pb-3 text-white after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-0 after:bg-[#1eca1b] after:transition-all after:duration-300 hover:after:w-[70%]">ALUMNOS</a>
+            <a 
+              href='' 
+              className="relative pb-3 text-white after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-0 after:bg-[#1eca1b] after:transition-all after:duration-300 hover:after:w-[70%]">
+                ALUMNOS</a>
           </li>
           <li className='relative p text-white'>
-            <a href='' className="relative pb-3 text-white after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-0 after:bg-[#1eca1b] after:transition-all after:duration-300 hover:after:w-[70%]">DOCENTES</a>
+            <a 
+              href='' 
+              className="relative pb-3 text-white after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-0 after:bg-[#1eca1b] after:transition-all after:duration-300 hover:after:w-[70%]">
+                DOCENTES</a>
           </li>
           <li className='relative p text-white'>
-            <a href='' className="relative pb-3 text-white after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-0 after:bg-[#1eca1b] after:transition-all after:duration-300 hover:after:w-[70%]">CARRERAS</a>
+            <a 
+              href='' 
+              className="relative pb-3 text-white after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-0 after:bg-[#1eca1b] after:transition-all after:duration-300 hover:after:w-[70%]">
+                CARRERAS</a>
           </li>
           <li className='relative p text-white'>
-            <a href='' className="relative pb-3 text-white after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-0 after:bg-[#1eca1b] after:transition-all after:duration-300 hover:after:w-[70%]">MATERIAS</a>
+            <a 
+              href='' 
+              className="relative pb-3 text-white after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-0.5 after:w-0 after:bg-[#1eca1b] after:transition-all after:duration-300 hover:after:w-[70%]">
+                MATERIAS</a>
           </li>
         
         </ul>
